df3a76a943c7a53ae4c55f4ba47f26550abe33ea
Maxim <87935999+realmizzer@users.noreply.github.com>
2025-05-22 05:34:14 +0700

    updated create_db.sql

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -1,53 +1,126 @@
--- Создание базы данных
-CREATE DATABASE TaxiDispatcher;
-USE TaxiDispatcher;
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `taxidispatcher`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: taxidispatcher
+-- ------------------------------------------------------
+-- Server version	9.3.0
 
--- Таблица водителей
-CREATE TABLE Drivers (
-    DriverID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    LicenseNumber VARCHAR(20) UNIQUE NOT NULL,
-    Phone VARCHAR(15) NOT NULL,
-    CarModel VARCHAR(50) NOT NULL,
-    CarNumber VARCHAR(15) UNIQUE NOT NULL,
-    Status ENUM('Available', 'OnRide', 'Offline') DEFAULT 'Available',
-    Rating DECIMAL(3,2) DEFAULT 0.0
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Таблица клиентов
-CREATE TABLE Clients (
-    ClientID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    Phone VARCHAR(15) UNIQUE NOT NULL,
-    RegistrationDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    LoyaltyPoints INT DEFAULT 0
-);
+--
+-- Table structure for table `clients`
+--
 
--- Таблица заказов
-CREATE TABLE Orders (
-    OrderID INT AUTO_INCREMENT PRIMARY KEY,
-    ClientID INT NOT NULL,
-    DriverID INT,
-    PickupAddress VARCHAR(100) NOT NULL,
-    DestinationAddress VARCHAR(100) NOT NULL,
-    OrderTime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    StartTime DATETIME,
-    EndTime DATETIME,
-    Status ENUM('Pending', 'Assigned', 'InProgress', 'Completed', 'Cancelled') DEFAULT 'Pending',
-    Price DECIMAL(10,2),
-    PaymentMethod ENUM('Cash', 'Card', 'MobilePay'),
-    FOREIGN KEY (ClientID) REFERENCES Clients(ClientID),
-    FOREIGN KEY (DriverID) REFERENCES Drivers(DriverID)
-);
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clients` (
+  `ClientID` int NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `RegistrationDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `LoyaltyPoints` int DEFAULT '0',
+  PRIMARY KEY (`ClientID`),
+  UNIQUE KEY `Phone` (`Phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Таблица платежей
-CREATE TABLE Payments (
-    PaymentID INT AUTO_INCREMENT PRIMARY KEY,
-    OrderID INT NOT NULL,
-    Amount DECIMAL(10,2) NOT NULL,
-    PaymentTime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Status ENUM('Pending', 'Completed', 'Failed') DEFAULT 'Pending',
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-);
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Sam','Serious','1','2025-05-15 06:11:54',0),(2,'Егор','Трофимов','+11111111','2025-05-15 07:07:21',0),(3,'aboba','aboba','645','2025-05-15 20:54:23',0),(4,'aaa','aaa','71','2025-05-22 01:19:16',0),(5,'11','11','71231231231','2025-05-22 01:22:25',0);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `drivers`
+--
+
+DROP TABLE IF EXISTS `drivers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `drivers` (
+  `DriverID` int NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `LicenseNumber` varchar(20) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `CarModel` varchar(50) NOT NULL,
+  `CarNumber` varchar(15) NOT NULL,
+  `Status` enum('Available','Busy','Offline') DEFAULT 'Available',
+  `Rating` decimal(3,2) DEFAULT '0.00',
+  PRIMARY KEY (`DriverID`),
+  UNIQUE KEY `LicenseNumber` (`LicenseNumber`),
+  UNIQUE KEY `CarNumber` (`CarNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drivers`
+--
+
+LOCK TABLES `drivers` WRITE;
+/*!40000 ALTER TABLE `drivers` DISABLE KEYS */;
+INSERT INTO `drivers` VALUES (14,'aboba','aboba','123','73123131231','123','A343RA343','Available',0.00);
+/*!40000 ALTER TABLE `drivers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `OrderID` int NOT NULL AUTO_INCREMENT,
+  `ClientID` int NOT NULL,
+  `DriverID` int DEFAULT NULL,
+  `PickupAddress` varchar(100) NOT NULL,
+  `DestinationAddress` varchar(100) NOT NULL,
+  `OrderTime` datetime DEFAULT CURRENT_TIMESTAMP,
+  `Status` enum('Pending','InProgress','Completed','Cancelled') DEFAULT 'Pending',
+  `Price` decimal(10,2) DEFAULT NULL,
+  `PaymentMethod` enum('Cash','Card','MobilePay') DEFAULT NULL,
+  PRIMARY KEY (`OrderID`),
+  KEY `ClientID` (`ClientID`),
+  KEY `DriverID` (`DriverID`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`ClientID`) REFERENCES `clients` (`ClientID`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`DriverID`) REFERENCES `drivers` (`DriverID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (5,1,14,'123','123','2025-05-22 05:06:56','Completed',NULL,'Cash');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-22  5:29:56
